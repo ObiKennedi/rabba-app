8d07cd93f2e691f27a22c7e5de8585c1841ed460
Obi Kennedy <166854306+ObiKennedi@users.noreply.github.com>
2025-11-08 01:30:56 +0100

    Link accounts

--- auth.ts
+++ auth.ts
@@ -11,14 +11,19 @@ export const {
     signIn,
     signOut
 } = NextAuth({
+    pages: {
+        signIn: "/login",
+        error: "/auth-error"
+    },
+    events: {
+        async linkAccount({ user }){
+            await db.user.update({
+                where: {id: user.id},
+                data: { emailVerified: new Date() }
+            })
+        }
+    },
     callbacks: {
-        async signIn({ user }) {
-            if (!user.id) return false;
-
-            //const existingUser = await getUserById(user.id);
-
-            return true
-        },
         async session({token, session}) {
             if (token.sub && session.user) {
                 session.user.id = token.sub;
